ce1e8521ca0b5c85fd05b54a160e074a8b007a47
Jakub Potocký <potockyjakub@gmail.com>
2023-09-03 17:39:25 +0200

    CS/typo fixes

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,9 @@
 
 All notable changes to `vue3-tabs-component` will be documented in this file
 
+## 1.3.6 - 2023-09-03
+- CS/typo fixes
+
 ## 1.3.5 - 2023-09-03
 - Refactor `Tab` & `Tabs` component prop definitions to TypeScript
 - Add `id` & `options.storageKey` props to `Tabs` component, which are used when remembering last opened tab
--- docs/App.vue
+++ docs/App.vue
@@ -4,49 +4,82 @@
   </h1>
   <tabs ref="testTabs">
     <tab name="First tab">
-      <h2 class="page-subtitle">First tab</h2>
+      <h2 class="page-subtitle">
+        First tab
+      </h2>
       This is the content of the first tab.
     </tab>
     <tab name="Second tab">
-      <h2 class="page-subtitle">Second tab</h2>
+      <h2 class="page-subtitle">
+        Second tab
+      </h2>
       This is the content of the second tab.
     </tab>
-    <tab name="Disabled tab" :is-disabled="true">
-      <h2 class="page-subtitle">Disabled tab</h2>
+    <tab
+      name="Disabled tab"
+      :is-disabled="true"
+    >
+      <h2 class="page-subtitle">
+        Disabled tab
+      </h2>
       This content will be unavailable while :is-disabled prop set to true
     </tab>
-    <tab id="oh-hi-mark" name="Custom fragment">
-      <h2 class="page-subtitle">Custom fragment</h2>
+    <tab
+      id="oh-hi-mark"
+      name="Custom fragment"
+    >
+      <h2 class="page-subtitle">
+        Custom fragment
+      </h2>
       The hash that is appended to the url can be customized.
     </tab>
-    <tab prefix="<span class='prefix'>→</span>" name="Prefix and suffix" suffix="<span class='suffix'>4</span>">
-      <h2 class="page-subtitle">Prefix and suffix</h2>
+    <tab
+      prefix="<span class='prefix'>→</span>"
+      name="Prefix and suffix"
+      suffix="<span class='suffix'>4</span>"
+    >
+      <h2 class="page-subtitle">
+        Prefix and suffix
+      </h2>
       A prefix and a suffix can be added — HTML allowed.
     </tab>
-    <tab nav-item-class="tabs-component-tab--custom" nav-item-link-class="tabs-component-tab-a--custom" name="Custom navigation item classes">
-      <h2 class="page-subtitle">Custom navigation item classes</h2>
+    <tab
+      nav-item-class="tabs-component-tab--custom"
+      nav-item-link-class="tabs-component-tab-a--custom"
+      name="Custom navigation item classes"
+    >
+      <h2 class="page-subtitle">
+        Custom navigation item classes
+      </h2>
       A custom nav-item-class & nav-item-link-class can be added to a tab's navigation item.
     </tab>
   </tabs>
 
   <section class="page-about">
-    <h2 class="page-subtitle">About</h2>
+    <h2 class="page-subtitle">
+      About
+    </h2>
     <p>
       The tab component above is powered by <a href="https://github.com/Jacobs63/vue3-tabs-component">jacobs63/vue3-tabs-component</a>.
     </p>
 
-    <p>The component will use the <strong>fragment</strong> of the url to choose which tab to open. So clicking
+    <p>
+      The component will use the <strong>fragment</strong> of the url to choose which tab to open. So clicking
       <a href="#second-tab">#second-tab</a>
       will display the contents of the tab named <code>Second tab</code>.</p>
 
-    <p>The component will also <strong>remember</strong>
+    <p>
+      The component will also <strong>remember</strong>
       which tab was opened previously. If you <a href="https://tabs-component.jakubpotocky.sk">reload without fragment</a>
       the tab that is currently active will receive focus again. More about this feature <a href="https://github.com/Jacobs63/vue3-tabs-component#remembering-the-last-opened-tab">on Github</a>.
     </p>
 
     <p>
       The active tab can also be changed programmatically outside:
-      <button class="tabs-component-btn" @click="changeToARandomTab">
+      <button
+        class="tabs-component-btn"
+        @click="changeToARandomTab"
+      >
         Change to a random tab
       </button>
     </p>
